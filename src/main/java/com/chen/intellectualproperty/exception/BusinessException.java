bf2abf47be0c22ce832a787cd1b6ed02472a4f41
chen <2249533492@qq.com>
2026-07-17 21:05:00 +0800

    修改登录注册

---
+++ src/main/java/com/chen/intellectualproperty/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.chen.intellectualproperty.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/chen/intellectualproperty/model/vo/UserVO.java
@@ -0,0 +1,11 @@
+package com.chen.intellectualproperty.model.vo;
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+    private Integer id;
+    private String email;
+    private String nickName;
+    private String token;
+}
